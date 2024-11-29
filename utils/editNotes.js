0303e1e4e75fd1b35348bd365f5956acbae7e2a3
Gabriel Siega <82450712+GabrielSiega@users.noreply.github.com>
2024-11-29 16:25:25 +0800

    Test commit

--- utils/editNotes.js
+++ utils/editNotes.js
@@ -1,23 +1,21 @@
 const { Notes } = require('../models/notes.js');
-const { readJSON } = require('./NotesUtils.js')
-const fs = require('fs').promises;
-//Edit notes //** */ Gab
+const { readJSON } = require('./NotesUtils.js');
+const fs = require('fs').promises; //Mocking File System Operations (fs)
+
+// Edit notes
 async function editNotes(req, res) {
     try {
-
         const id = req.params.id;
         const title = req.body.title;
         const description = req.body.description;
         const priority = req.body.priority;
 
-
         const allNotes = await readJSON('utils/notes.json');
-        var modified = false;
+        let modified = false;
 
-        for (var i = 0; i < allNotes.length; i++) {
-            var currentResource  = allNotes[i];
-            if (currentResource .id == id) {
-
+        for (let i = 0; i < allNotes.length; i++) {
+            const currentResource = allNotes[i];
+            if (currentResource.id == id) {
                 allNotes[i].title = title;
                 allNotes[i].description = description;
                 allNotes[i].priority = priority;
@@ -25,12 +23,13 @@ async function editNotes(req, res) {
                 break;
             }
         }
+
         if (modified) {
             await fs.writeFile('utils/notes.json', JSON.stringify(allNotes), 'utf8');
             return res.status(201).json({ message: 'Note Updated successfully!' });
         } else {
-            return res.status(500).json({ message: 'Error occurred, unable to modify!' });
-
+            // Change to 404 Not Found
+            return res.status(404).json({ message: 'Note not found!' });
         }
     } catch (error) {
         return res.status(500).json({ message: error.message });
@@ -39,4 +38,4 @@ async function editNotes(req, res) {
 
 module.exports = {
     editNotes
-}
+};
