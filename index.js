715f82d8e1ed73fd8c3ba4e28f33bd0aa2c7102f
Gabriel Siega <Gabriel.siega@yahoo.com>
2025-02-08 23:32:43 +0800

    enabled testing for analytics

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const { addNotes } = require('./utils/addNotes.js')
 const { viewNotes } = require('./utils/viewNotes.js')
 const { editNotes } = require('./utils/editNotes.js')
 const { deleteNotes } = require('./utils/deleteNotes.js')
+
+const logger = require('./logger');
+
 var app = express();
 const PORT = process.env.PORT || 5050
 var startPage = "index.html";
@@ -11,6 +14,9 @@ var startPage = "index.html";
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("./public"));
+
+const statusMonitor = require('express-status-monitor');
+app.use(statusMonitor()); // Real Time Monitoring
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/public/" + startPage);
@@ -25,6 +31,8 @@ server = app.listen(PORT, function () {
     const baseUrl = `http://${address.address == "::" ? 'localhost' :
         address.address}:${address.port}`;
     console.log(`Daily Dose Project: ${baseUrl}`);
+    logger.info(`Daily Dose Project: ${baseUrl}!`);
+    logger.error(`Example or error log`)
 });
 
 module.exports = { app, server } 
